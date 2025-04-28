ae4df4c16e039aa649f0d5b53bf9bc70b0f27339
SSY199 <s.s.y.9321423@gmail.com>
2025-04-28 12:38:09 +0530

    Environment Setup

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+ import React from 'react'
+import { Button } from './components/ui/button'
+ 
+ const App = () => {
+   return (
+     <div>
+      <Button variant="default">Hello World</Button>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+     </div>
+   )
+ }
+ 
+ export default App
